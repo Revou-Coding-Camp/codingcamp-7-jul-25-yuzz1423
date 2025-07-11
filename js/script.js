6b669e8b7718551a389e5a4b66dbaad2cd1e9f49
yuzz1423 <yuann1423@gmail.com>
2025-07-11 16:12:17 +0700

    komit kesepuluh

--- js/script.js
+++ js/script.js
@@ -155,3 +155,10 @@ clearBtn.addEventListener('click', function () {
         renderList(todos);
     }
 });
+
+const toggleBtn = document.getElementById("mode-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  toggleBtn.textContent = document.body.classList.contains("dark-mode") ? "Light" : "Dark";
+});
