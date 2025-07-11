0ce65d10f27835b021ca5556ad6ee26e85e8f712
yuzz1423 <yuann1423@gmail.com>
2025-07-11 22:51:24 +0700

    fitur selesai

--- js/script.js
+++ js/script.js
@@ -162,3 +162,4 @@ toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   toggleBtn.textContent = document.body.classList.contains("dark-mode") ? "Light" : "Dark";
 });
+
